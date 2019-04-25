8c3a7a19497f585e2c5bf16ca131ba775f49b408
Owain Roberts <owain.roberts@bjss.com>
2019-04-25 17:02:46 +0100

    EE-16843 Added health endpoint

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.SERVER_PORT || '8000';
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/healthz', function (req, res) {
+  res.send({env: process.env.ENV, status: 'OK'})
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    console.log('ui on:' + port)
+})
