c59a481455dd40258b8067131997e2a793b46ef9
Owain Roberts <owain.roberts@bjss.com>
2019-04-26 14:42:47 +0100

    EE-16843 Fixing lint issue in server.test.js

--- server.test.js
+++ server.test.js
@@ -1,18 +1,18 @@
 'use strict';
 const request = require('supertest');
 
-describe('Stats UI server', function () {
+describe('Stats UI server', function() {
     var server;
     beforeEach(function() {
         server = require('./server');
     });
     afterEach(function() {
         server.close();
-    })
+    });
 
     it('responds to the health endpoint', function testHealthEndpoint(done) {
         request(server)
             .get('/healthz')
             .expect(200, done);
-    })
-})
+    });
+});
