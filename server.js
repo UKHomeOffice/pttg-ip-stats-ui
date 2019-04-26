a71bdf33696c3d9185a1c71f15d9c177cb9d71fc
Owain Roberts <owain.roberts@bjss.com>
2019-04-26 12:00:02 +0100

    EE-16843 Made the URL of the IP API configurable using an Environment Variable

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const request = require('request');
 
 const app = express();
-const port = process.env.SERVER_PORT || '8000';
+const PORT = process.env.SERVER_PORT || '8000';
+const API_ROOT = process.env.API_ROOT || 'http://localhost:8081/';
 
 app.get('/healthz', function(req, res) {
     res.send({ env: process.env.ENV, status: 'OK' })
@@ -11,7 +12,7 @@ app.get('/healthz', function(req, res) {
 app.get('/', function(req, res) {
     const requestOptions = {
         url: '/statistics',
-        baseUrl: 'http://localhost:8081/',
+        baseUrl: API_ROOT,
         qs: req.query,
         headers: {
             'x-auth-username': req.get('x-auth-username'),
@@ -23,8 +24,8 @@ app.get('/', function(req, res) {
     })
 })
 
-const server = app.listen(port, () => {
-    console.log('ui on:' + port)
+const server = app.listen(PORT, () => {
+    console.log('ui on:' + PORT)
 })
 
 module.exports = server;
