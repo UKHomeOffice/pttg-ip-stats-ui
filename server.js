d355b3a57859306cd07a276f41b1e4eae51d69ab
Owain Roberts <owain.roberts@bjss.com>
2019-04-26 10:23:48 +0100

    EE-16843 Made the UI return the result from its upstream API call to the caller, although the API is not setup

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const request = require('request');
 
 const app = express();
 const port = process.env.SERVER_PORT || '8000';
 
-app.get('/healthz', function (req, res) {
-  res.send({env: process.env.ENV, status: 'OK'})
+app.get('/healthz', function(req, res) {
+    res.send({ env: process.env.ENV, status: 'OK' })
+})
+
+app.get('/', function(req, res) {
+    request({}, (error, response, body) => {
+        res.send(body);
+    })
 })
 
 const server = app.listen(port, () => {
--- server.test.js
+++ server.test.js
@@ -1,17 +1,40 @@
+const mockRequire = require('mock-require');
 const request = require('supertest');
 
-describe('Stats UI server', function () {
+const MOCK_CSV_RESPONSE = `From Date,To Date,Total Requests,Passed,Not Passed,Not Found,Error
+2019-01-01, 2019-01-31, 20, 15, 2, 2, 1`
+
+describe('Stats UI server', function() {
     var server;
     beforeEach(function() {
         server = require('./server');
     });
     afterEach(function() {
         server.close();
-    })
+    });
 
     it('responds to the health endpoint', function testHealthEndpoint(done) {
         request(server)
             .get('/healthz')
             .expect(200, done);
-    })
+    });
+
+    it('returns not found for unknown routes', function testNotFound(done) {
+        request(server)
+            .get('/anyunknownroute')
+            .expect(404, done);
+    });
+
+    it('forwards requests to / on to IP API', function testForwarding(done) {
+        var capturedOptions;
+        mockRequire('request', function(options, callback) {
+            capturedOptions = options;
+            callback('', '', MOCK_CSV_RESPONSE);
+        })
+        server = mockRequire.reRequire('./server');
+        // verifyOptions(capturedOptions);
+        request(server)
+            .get('/?taxYear=2018/2019')
+            .expect(200, MOCK_CSV_RESPONSE, done);
+    });
 })
